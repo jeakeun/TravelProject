63d07c4459327daa93ed21f141faa7d59ef50811
parkha-1 <darkpiao@naver.com>
2026-02-22 14:22:01 +0900

    메인화면 수정

--- community_react/src/index.js
+++ community_react/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 🚩 추가
 import App from './App'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // 🚩 조회수가 2씩 오르는 현상을 해결하기 위해 StrictMode를 주석 처리합니다.
+  // 🚩 조회수 중복 방지를 위해 StrictMode는 주석 유지
   // <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* 🚩 App을 BrowserRouter로 감싸야 라우팅 훅이 작동합니다 */}
+      <App />
+    </BrowserRouter>
   // </React.StrictMode>
 );
