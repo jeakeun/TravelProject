a43a53dd4e0591d43fb7c9db7de12fd6354f1829
netizen368 <rlawlsdud20302@gmail.com>
2026-03-03 14:52:37 +0900

    파비콘 에러 수정

--- community_react/src/components/Header.jsx
+++ community_react/src/components/Header.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { getNickname, isAdmin } from "../utils/user";
 import ProfileImage from "./ProfileImage";
-import FaviconLogo from "../assets/favicon/Favicon.png";
 
 const translations = {
   KR: {
@@ -61,7 +60,7 @@ function Header({ user, onLogout, openLogin, openSignup, currentLang, setCurrent
           <span
             className="logo-icon"
             aria-hidden="true"
-            style={{ backgroundImage: `url(${FaviconLogo})` }}
+            style={{ backgroundImage: "url(/assets/Favicon.png)" }}
           />
           <span className="logo-text">TRAVEL</span>
         </Link>
